db1f12df0af85a36878e188b500abc2fa017136e
unknown <lidong@lidong-PC.(none)>
2013-06-10 11:33:50 +0800

    update

--- MyAspwork/Sqlconnection/DBconnection.cs
+++ MyAspwork/Sqlconnection/DBconnection.cs
@@ -15,24 +15,24 @@ namespace MyAspwork.Sqlconnection
             myConnection = new MySqlConnection(System.Configuration.ConfigurationManager.AppSettings["ConnectionString"].ToString());  
             return myConnection;
         }
+       //fsafsafsfsfdsfsf
 
-
-        //string query = "select * from guestbook";
-        //MySqlConnection myConnection = new MySqlConnection("server=localhost;user id=root;password=;database=guestbook"); 
-        //MySqlCommand myCommand=new MySqlCommand(query,myConnection);
-        //myConnection.Open();
-        //myCommand.ExecuteNonQuery();
-        //MySqlDataReader myDataReader = myCommand.ExecuteReader();
-        //string bookres="";
-        //while (myDataReader.Read()==true)
-        //{
-        //    bookres+=myDataReader["id"];
-        //    bookres+=myDataReader["user"];
-        //    bookres += myDataReader["pass"];
-        //}
-        //myDataReader.Close();
-        //myConnection.Close();
-        //lb1.Text = bookres;      
+      //string query = "select * from guestbook";
+      //MySqlConnection myConnection = new MySqlConnection("server=localhost;user id=root;password=;database=guestbook"); 
+      //MySqlCommand myCommand=new MySqlCommand(query,myConnection);
+      //myConnection.Open();
+      //myCommand.ExecuteNonQuery();
+      //MySqlDataReader myDataReader = myCommand.ExecuteReader();
+      //string bookres="";
+      //while (myDataReader.Read()==true)
+      //{
+      //    bookres+=myDataReader["id"];
+      //    bookres+=myDataReader["user"];
+      //    bookres += myDataReader["pass"];
+      //}
+      //myDataReader.Close();
+      //myConnection.Close();
+      //lb1.Text = bookres;      
 
 
         
